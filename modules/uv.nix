66a6f7d22467470fdddf16e9ea94b83c02185a57
cake <zhaocake@foxmail.com>
2026-01-01 21:53:16 +0800

    fix(python): change source to tsinghua mirror.

--- modules/uv.nix
+++ modules/uv.nix
@@ -5,7 +5,7 @@
   # 位于 ~/.config/uv/uv.toml
   xdg.configFile."uv/uv.toml".text = ''
     [[index]]
-    url = "https://pypi.mirrors.ustc.edu.cn/simple"
+    url = "https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple/"
     default = true
   '';
 }
