110c744abead5f7edf2f02a7de42ae6c0c63c2db
cake <zhaocake@foxmail.com>
2026-02-25 21:15:31 +0800

    feat: add new machine matebook.

--- flake.nix
+++ flake.nix
@@ -74,6 +74,38 @@
             }
           ];
         };
+
+      # ==========================================
+      # 场景 3: Matebook 笔记本
+      # ==========================================
+      # 使用命令: sudo nixos-rebuild switch --flake .#matebook
+        matebook = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
+          
+          modules = [
+            # 1. 导入这台机器特有的配置
+            ./hosts/matebook/configuration.nix
+
+            # 2. 将 Home Manager 作为 NixOS 模块加载
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              
+              # 复用根目录下的 home.nix
+              home-manager.users.cake = {
+                imports = [
+                  ./home.nix
+                  nixvim.homeModules.nixvim
+                ];
+              };
+
+              # 传递 inputs 给 home.nix
+              home-manager.extraSpecialArgs = { inherit inputs; };
+            }
+          ];
+        };
       };
     };
 }
