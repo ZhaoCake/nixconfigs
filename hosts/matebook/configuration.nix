26109dab15bfc137611b7b3a164fcbdc060be07c
cake <zhaocake@foxmail.com>
2026-02-27 11:42:32 +0800

    fix(sound): use pulseaudio but not pipewire.

--- hosts/matebook/configuration.nix
+++ hosts/matebook/configuration.nix
@@ -73,21 +73,10 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
+  # Enable sound with Pulseaudio.
+  hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  services.pipewire.enable = false;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -142,8 +131,7 @@
     vscode
     clash-verge-rev
     remmina
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    cherry-studio
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
